17a697f095f85f925b707f20d1f7ef80e8c22392
Giselle Souza <reallovetour@gmail.com>
2023-04-19 13:56:00 -0300

    Criada a Model Categoria e Produto

---
+++ src/main/java/com/generation/lojagames/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.lojagames.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Categoria {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@NotBlank (message= "O atributo 'tipo' é obrigatório!")
+		@Size(min = 5, max = 255, message = "O atributo 'tipo' deve ter no mínimo 5 e no máximo 255 caracteres")
+		private String tipo;
+
+		//Get e Set
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+		
+		
+}
